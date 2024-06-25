d1b5e7eedd8eb2cfbaf3a3fbfc260bc4f4aeff52
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-25 18:30:12 +0200

    feat: create pivot table between table projects and technologies

---
+++ database/migrations/2024_06_25_161420_create_project_technology_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+            //tabella ponte
+            $table->unsignedBigInteger('project_id');
+            //cascadeOnDelete serve per cancellare il record nella tabella ponte
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('technology_id');
+            $table->foreign('technology_id')->references('id')->on('technology')->cascadeOnDelete();
+
+            $table->primary(['project_id', 'technology_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_technology');
+    }
+};
